741fb4d1485a540499e84eef1f9ec9992c265591
dineshagrawal2468 <dineshagrawal.9371@gmail.com>
2026-04-04 18:24:28 +0530

    Update Pelindrome.c

--- Pelindrome.c
+++ Pelindrome.c
@@ -3,7 +3,9 @@
 int main()
 {
     int num, original, reverse = 0, digit;
+    int i, j;
 
+    // Palindrome check
     printf("Enter a five digit number: ");
     scanf("%d", &num);
 
@@ -17,9 +19,23 @@ int main()
     }
 
     if(original == reverse)
-        printf("The number is a Palindrome.\n");
+        printf("The number is Palindrome\n");
     else
-        printf("The number is not a Palindrome.\n");
+        printf("The number is not Palindrome\n");
+
+    // Pattern printing
+    printf("\nPattern:\n");
+
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = i; j >= 1; j--)
+            printf("%d ", j);
+
+        for(j = 2; j <= i; j++)
+            printf("%d ", j);
+
+        printf("\n");
+    }
 
     return 0;
 }
